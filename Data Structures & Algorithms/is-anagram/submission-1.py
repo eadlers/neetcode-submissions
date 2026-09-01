f43618d0751a1984b831df22fce7b2ab776d9567
Erik Adler <eadlers@hotmail.com>
2026-09-01 08:50:11 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #hashmap solution
+        #iterate over s and then over t
+        #use each char as key, count as value
+        #s increases count, t decreases
+        #each key must have 0 as value after all chars are iterated
+        anagramDict = {}
+        for x in s:
+            anagramDict[x] = anagramDict.get(x, 0) + 1
+        for y in t:
+            anagramDict[y] = anagramDict.get(y, 0) - 1
+        for count in anagramDict.values():
+            if count != 0:
+                return False
+        return True
+        
